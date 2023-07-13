a3f66406f2ab7478efdf2f4f77381f4e8a862595
mucabo <murillodomingos516@gmail.com>
2023-07-13 17:30:29 -0300

    Finalizado

--- loja.c
+++ loja.c
@@ -1,22 +1,32 @@
+/* TRABALHO ICC 2023.1 - PROF. RUDNEI
+
+    INTEGRANTES: -BRUNO NEVES BOA SORTE 14562528
+                 -CAIO URAMOTO EVANGELISTA 14777351
+                 -MURILLO DOMINGOS DE ALMEIDA 14804083
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// definicao da struct
 typedef struct loja
 {
     int id, qtd;
-    float preço;
+    float preco;
     char nome[50];
 } Estoque;
 
+// rotina de inserção de novos produtos
 void insere_produto(Estoque *vet, int indice)
 {
     vet[indice].id = indice;
     scanf("%s", vet[indice].nome);
     scanf("%d", &vet[indice].qtd);
-    scanf("%f", &vet[indice].preço);
+    scanf("%f", &vet[indice].preco);
 }
 
+// rotina de aumentar o numero de itens de certo produto
 void aumenta_estoque(Estoque *vet)
 {
     int indice, aumento;
@@ -25,13 +35,15 @@ void aumenta_estoque(Estoque *vet)
     vet[indice].qtd += aumento;
 }
 
+// rotina que modifica o preco de determinado produto
 void modifica_preco(Estoque *vet)
 {
     int indice;
     scanf("%d", &indice);
-    scanf("%f", &vet[indice].preço);
+    scanf("%f", &vet[indice].preco);
 }
 
+// rotina que consulta o estoque do mercado
 void consulta_estoque(Estoque *vet, int indice)
 {
     for (int i = 0; i < indice; i++)
@@ -41,17 +53,20 @@ void consulta_estoque(Estoque *vet, int indice)
     printf("--------------------------------------------------\n");
 }
 
+// rotina que consulta o saldo em caixa
 void consulta_saldo(float saldo)
 {
     printf("Saldo: %.2f\n", saldo);
     printf("--------------------------------------------------\n");
 }
 
+// rotina de venda dos produtos
 void venda(Estoque *vet, float *saldo)
 {
-    float somapreço = 0;
+    float somapreco = 0;
     int indice;
 
+    // loop de venda terminado em -1
     while(1)
     {
         scanf("%d", &indice);
@@ -69,27 +84,57 @@ void venda(Estoque *vet, float *saldo)
         }
         else
         {
-            printf("%s %.2f\n", vet[indice].nome, vet[indice].preço);
-            somapreço += vet[indice].preço;
+            printf("%s %.2f\n", vet[indice].nome, vet[indice].preco);
+            somapreco += vet[indice].preco;
         }
     }
-    *saldo += somapreço; 
-    printf("Total: %.2f\n", somapreço);
+    *saldo += somapreco; 
+    printf("Total: %.2f\n", somapreco);
     printf("--------------------------------------------------\n");
 }
 
 int main(void)
 {
-    int produtos_iniciais, indice = 0, count = 1;
+    // definicao das variaveis
+    int produtos_iniciais, indice = 0, count = 1, linha = 0;
     float saldo;
-    char sigla[2];
-    Estoque *vet;
+    char sigla[2], linha_arquivo[100];;
 
-    scanf("%d", &produtos_iniciais);
-    vet = (Estoque*)malloc(produtos_iniciais * sizeof(Estoque));
+    Estoque *vet = NULL;
+    FILE *fp;
 
-    scanf("%f", &saldo);
+    // caso o arquivo nao esteja sao puxados os dados dele 
+    if(((fp = fopen("estoque.txt", "r"))) != NULL)
+    {
+        while(fgets(linha_arquivo, sizeof(linha_arquivo), fp) != NULL)
+        {
+            linha++;
+        }
 
+        indice = linha - 1;
+        produtos_iniciais = linha -1;
+        vet = (Estoque*)malloc(indice * sizeof(Estoque));
+
+        fseek(fp, 0, SEEK_SET);
+        // linha 1 saldo
+        fscanf(fp,"%f", &saldo);
+        // linha 2 em diante produtos
+        for(int i = 0; i < linha - 1; i++)
+        {
+            fscanf(fp, "%d %s %d %f", &vet[i].id, vet[i].nome, &vet[i].qtd, &vet[i].preco);
+        }
+
+        fclose(fp);
+    }
+    // caso nao segue execucao perguntadno a qtd de produtos e o saldo inicial em caixa
+    else
+    {
+        scanf("%d", &produtos_iniciais);
+        vet = (Estoque*)malloc(produtos_iniciais * sizeof(Estoque));
+        scanf("%f", &saldo);
+    }
+
+    // verificacao dos comandos
     while(1)
     {
         scanf(" %s", sigla);
@@ -102,6 +147,12 @@ int main(void)
         {
             insere_produto(vet, indice);
             indice++;
+
+            if(indice == produtos_iniciais)
+            {
+                vet = (Estoque*)realloc(vet, (produtos_iniciais + 10 * count) * sizeof(Estoque));
+                count++;
+            }
         }
         else if(strcmp(sigla, "AE") == 0)
         {
@@ -123,11 +174,18 @@ int main(void)
         {
             consulta_saldo(saldo);
         }
-
-        if(indice > produtos_iniciais)
-        {
-            vet = (Estoque*)realloc(vet, produtos_iniciais + 10 * count * sizeof(Estoque));
-            count++;
-        }
     }
+
+    // imputa no arquivo as alterações feitas
+    fp = fopen("estoque.txt", "w");
+
+    fprintf(fp, "%f\n", saldo);
+    for(int i = 0; i < indice; i++)
+    {
+        fprintf(fp, "%d %s %d %f\n", vet[i].id, vet[i].nome, vet[i].qtd, vet[i].preco);
+    }
+    fclose(fp);
+
+    free(vet);
+    vet = NULL;
 }
